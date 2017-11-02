4b1b51ad79c88967fd2b7f21296cafc26a942416
Anastasiya Kastsiukova <anastasiya_kastsiukova@epam.com>
2017-11-02 11:42:59 +0300

    Added exception handling task

---
+++ 2_Exception_handling/Show_First_Symbol/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Show_First_Symbol
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+
+      DateTime dat = DateTime.Now;
+
+      Console.WriteLine("\nToday is {0:d} at {0:T}.", dat);
+      Console.Write("\nEnter any string: ");
+      string s = null;
+      s = Console.ReadLine();
+      string res = string.IsNullOrEmpty(s) ? null : s[0].ToString();
+      Console.WriteLine("Your string is: {0} \nThank you =)", s[0]);
+    }
+  }
+}
