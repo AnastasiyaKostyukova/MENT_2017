2958872fb782b7ec3b1df1105763fc8637dc4501
Anastasiya Kastsiukova <anastasiya_kastsiukova@epam.com>
2018-01-26 15:50:25 +0300

    11_Tools_4_developer module

---
+++ 11_Tools_4_developer/StyleCop.NastyaRules/ControllerPostfixRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using StyleCop.CSharp;
+
+namespace StyleCop.NastyaRules
+{
+  using System.Web.Mvc;
+
+  [SourceAnalyzer(typeof(CsParser))]
+  public class ControllerPostfixRule : SourceAnalyzer
+  {
+    public override void AnalyzeDocument(CodeDocument document)
+    {
+      CsDocument csDocument = (CsDocument)document;
+      csDocument.WalkDocument(new CodeWalkerElementVisitor<object>(this.VisitElement));
+    }
+
+    private bool VisitElement(CsElement element, CsElement parentElement, object context)
+    {
+      var controller = element as Class;
+      if (controller != null )//&&*/ element.GetType().BaseType == typeof(Controller))
+      {
+        if (!element.Name.EndsWith("NastyaController", StringComparison.Ordinal))
+        {
+          this.AddViolation(element, "ControllerPostfixRule");
+        }
+
+        return false;
+      }
+      return true;
+    }
+  }
+}
